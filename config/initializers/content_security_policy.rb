515a0719001b8b876d32ef794e88b9212489a700
Eskandar <isgan151@gmail.com>
2025-10-30 19:32:39 +0000

    allowing application.config.content_security_policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,25 +1,14 @@
-# Be sure to restart your server when you modify this file.
+# config/initializers/content_security_policy.rb
 
-# Define an application-wide content security policy.
-# See the Securing Rails Applications Guide for more information:
-# https://guides.rubyonrails.org/security.html#content-security-policy-header
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data
+  policy.object_src  :none
+  policy.script_src  :self, :https, "https://s3.tradingview.com", "https://www.tradingview.com"
+  policy.style_src   :self, :https, :unsafe_inline
+  policy.connect_src :self, :https
+  policy.frame_src   :self, :https, "https://www.tradingview.com"
+  policy.connect_src :self, :https, "https://api.coingecko.com"
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+end

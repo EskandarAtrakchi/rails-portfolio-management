1374822f2310ee4fa4fe40400bc7b0b86f41e228
Eskandar <isgan151@gmail.com>
2025-11-03 11:04:04 +0000

    clarification added

--- test/controllers/cryptos_controller_test.rb
+++ test/controllers/cryptos_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class CryptosControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    # getting the URL for the index action of CryptosController
     get cryptos_index_url
     assert_response :success
   end

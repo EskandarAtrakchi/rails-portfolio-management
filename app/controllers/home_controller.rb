1a9928afc13178edd370404b0cf981f5fa729317
Eskandar <isgan151@gmail.com>
2025-11-03 10:00:42 +0000

    home controller API pull data and implement math on the data to display on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,15 @@
 class HomeController < ApplicationController
   def index
+    # Show total portfolio value and count if user is signed in
     if user_signed_in?
+      # Calculate total portfolio value and count by using simple math on portfolios
       @total_value = current_user.portfolios.sum("quantity * price")
+      # Get the number of unique cryptocurrencies in the portfolio
       @portfolio_count = current_user.portfolios.count
     end
   end
 
+  # About page might not be needed, I will decide later 
   def about
   end
 end

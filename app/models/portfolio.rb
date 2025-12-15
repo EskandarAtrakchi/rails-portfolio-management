8c2e35fe080a4bd42304b93a264c914fa38eebc1
EskandarAtrakchi <isgan151@gmail.com>
2025-12-15 12:59:44 +0000

    ' removed and routes fixed

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,6 +5,4 @@ class Portfolio < ApplicationRecord
   validates :symbol, presence: true
   validates :quantity, presence: true,
                        numericality: { greater_than: 0, message: "must be a positive number" }
-  # ⛔ Remove or comment out this line:
-  # validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios
+  # Define routes first so they take precedence
   get "cryptos/index"
   get "sessions/new"
   get "sessions/create"
@@ -7,15 +8,17 @@ Rails.application.routes.draw do
   get "home/index"
   get "home/about"
   root "home#index"
-  
+  # User routes
   resources :users
   
-  # authentication routes (to be created)
+  # authentication routes
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-  
+  # registration routes
   get "register", to: "users#new"
+  # Create route for cryptocurrencies index
   get "cryptos", to: "cryptos#index"
+  # About page route (not sure if I will add about page) but I'll keep it for now
   get "about", to: "about#home"
 end

6e88cbccf2cf50c7bfd3b0b3c401c1e3efa37e86
EskandarAtrakchi <isgan151@gmail.com>
2025-12-15 12:42:25 +0000

    clarifications added to the code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :user_signed_in?
-
+  # make current_user and user_signed_in? available in views
   def current_user
     #try to find the user by user ID if session user ID returns (true)
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
-
+  # check if a user is signed in
   def user_signed_in?
     # check if current_user is present (true/false)
     current_user.present?
   end
-
+  
   def authenticate_user!
     # redirect to login page if user is not signed in 
     unless user_signed_in?

265e76e34118c28c7df74e13e272a6e738c6dec6
EskandarAtrakchi <isgan151@gmail.com>
2025-12-15 12:47:44 +0000

    User profile management clarifications added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,14 @@ class UsersController < ApplicationController
   before_action :authorize_user!, only: %i[edit update destroy]
 
   def show; end
-
+  # GET /users/new
   def new
     @user = User.new
   end
-
+  # POST /users
   def create
     @user = User.new(user_params)
+    # Attempt to save the new user
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path, notice: "Account created successfully!"
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-
+  # GET /users/1/edit
   def edit; end
 
  def update
@@ -30,23 +31,23 @@ class UsersController < ApplicationController
     end
   end
 
-
+  # DELETE /users/1
   def destroy
     @user.destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Account deleted."
   end
-
+  
   private
-
+    
     def set_user
       @user = User.find(params[:id])
     end
-
+    # Ensure only the profile owner can edit, update, or destroy their profile
     def authorize_user!
       redirect_to root_path, alert: "You can only edit your own profile." unless @user == current_user
     end
-
+    # Strong parameters for user
     def user_params
       params.require(:user).permit(
         :first_name, :last_name, :username, :email,
